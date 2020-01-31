e255022d8f1ef5bd8903e5db3814e5875e808b4d
Riobard <me@riobard.com>
2020-01-31 18:10:07 +0800

    Drop deprecated stream ciphers

--- core/cipher.go
+++ core/cipher.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	"github.com/riobard/go-shadowsocks2/shadowaead"
-	"github.com/riobard/go-shadowsocks2/shadowstream"
 )
 
 type Cipher interface {
@@ -38,28 +37,10 @@ var aeadList = map[string]struct {
 	"AEAD_CHACHA20_POLY1305": {32, shadowaead.Chacha20Poly1305},
 }
 
-// List of stream ciphers: key size in bytes and constructor
-var streamList = map[string]struct {
-	KeySize int
-	New     func(key []byte) (shadowstream.Cipher, error)
-}{
-	"AES-128-CTR":   {16, shadowstream.AESCTR},
-	"AES-192-CTR":   {24, shadowstream.AESCTR},
-	"AES-256-CTR":   {32, shadowstream.AESCTR},
-	"AES-128-CFB":   {16, shadowstream.AESCFB},
-	"AES-192-CFB":   {24, shadowstream.AESCFB},
-	"AES-256-CFB":   {32, shadowstream.AESCFB},
-	"CHACHA20-IETF": {32, shadowstream.Chacha20IETF},
-	"XCHACHA20":     {32, shadowstream.Xchacha20},
-}
-
 // ListCipher returns a list of available cipher names sorted alphabetically.
 func ListCipher() []string {
 	var l []string
 	for k := range aeadList {
-		l = append(l, k)
-	}
-	for k := range streamList {
 		l = append(l, k)
 	}
 	sort.Strings(l)
@@ -94,17 +75,6 @@ func PickCipher(name string, key []byte, password string) (Cipher, error) {
 		return &aeadCipher{aead}, err
 	}
 
-	if choice, ok := streamList[name]; ok {
-		if len(key) == 0 {
-			key = kdf(password, choice.KeySize)
-		}
-		if len(key) != choice.KeySize {
-			return nil, shadowstream.KeySizeError(choice.KeySize)
-		}
-		ciph, err := choice.New(key)
-		return &streamCipher{ciph}, err
-	}
-
 	return nil, ErrCipherNotSupported
 }
 
@@ -113,13 +83,6 @@ type aeadCipher struct{ shadowaead.Cipher }
 func (aead *aeadCipher) StreamConn(c net.Conn) net.Conn { return shadowaead.NewConn(c, aead) }
 func (aead *aeadCipher) PacketConn(c net.PacketConn) net.PacketConn {
 	return shadowaead.NewPacketConn(c, aead)
-}
-
-type streamCipher struct{ shadowstream.Cipher }
-
-func (ciph *streamCipher) StreamConn(c net.Conn) net.Conn { return shadowstream.NewConn(c, ciph) }
-func (ciph *streamCipher) PacketConn(c net.PacketConn) net.PacketConn {
-	return shadowstream.NewPacketConn(c, ciph)
 }
 
 // dummy cipher does not encrypt

c03a025f8f9a64500dc1708464ba924e2759e48f
Riobard <me@riobard.com>
2020-02-20 17:41:54 +0800

    Deprecate all stream ciphers
    And AES-192-GCM which no one ever uses anyway.

--- core/cipher.go
+++ core/cipher.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	"github.com/shadowsocks/go-shadowsocks2/shadowaead"
-	"github.com/shadowsocks/go-shadowsocks2/shadowstream"
 )
 
 type Cipher interface {
@@ -29,7 +28,6 @@ var ErrCipherNotSupported = errors.New("cipher not supported")
 
 const (
 	aeadAes128Gcm        = "AEAD_AES_128_GCM"
-	aeadAes192Gcm        = "AEAD_AES_192_GCM"
 	aeadAes256Gcm        = "AEAD_AES_256_GCM"
 	aeadChacha20Poly1305 = "AEAD_CHACHA20_POLY1305"
 )
@@ -40,33 +38,14 @@ var aeadList = map[string]struct {
 	New     func([]byte) (shadowaead.Cipher, error)
 }{
 	aeadAes128Gcm:        {16, shadowaead.AESGCM},
-	aeadAes192Gcm:        {24, shadowaead.AESGCM},
 	aeadAes256Gcm:        {32, shadowaead.AESGCM},
 	aeadChacha20Poly1305: {32, shadowaead.Chacha20Poly1305},
-}
-
-// List of stream ciphers: key size in bytes and constructor
-var streamList = map[string]struct {
-	KeySize int
-	New     func(key []byte) (shadowstream.Cipher, error)
-}{
-	"AES-128-CTR":   {16, shadowstream.AESCTR},
-	"AES-192-CTR":   {24, shadowstream.AESCTR},
-	"AES-256-CTR":   {32, shadowstream.AESCTR},
-	"AES-128-CFB":   {16, shadowstream.AESCFB},
-	"AES-192-CFB":   {24, shadowstream.AESCFB},
-	"AES-256-CFB":   {32, shadowstream.AESCFB},
-	"CHACHA20-IETF": {32, shadowstream.Chacha20IETF},
-	"XCHACHA20":     {32, shadowstream.Xchacha20},
 }
 
 // ListCipher returns a list of available cipher names sorted alphabetically.
 func ListCipher() []string {
 	var l []string
 	for k := range aeadList {
-		l = append(l, k)
-	}
-	for k := range streamList {
 		l = append(l, k)
 	}
 	sort.Strings(l)
@@ -84,8 +63,6 @@ func PickCipher(name string, key []byte, password string) (Cipher, error) {
 		name = aeadChacha20Poly1305
 	case "AES-128-GCM":
 		name = aeadAes128Gcm
-	case "AES-192-GCM":
-		name = aeadAes192Gcm
 	case "AES-256-GCM":
 		name = aeadAes256Gcm
 	}
@@ -101,17 +78,6 @@ func PickCipher(name string, key []byte, password string) (Cipher, error) {
 		return &aeadCipher{aead}, err
 	}
 
-	if choice, ok := streamList[name]; ok {
-		if len(key) == 0 {
-			key = kdf(password, choice.KeySize)
-		}
-		if len(key) != choice.KeySize {
-			return nil, shadowstream.KeySizeError(choice.KeySize)
-		}
-		ciph, err := choice.New(key)
-		return &streamCipher{ciph}, err
-	}
-
 	return nil, ErrCipherNotSupported
 }
 
@@ -122,15 +88,7 @@ func (aead *aeadCipher) PacketConn(c net.PacketConn) net.PacketConn {
 	return shadowaead.NewPacketConn(c, aead)
 }
 
-type streamCipher struct{ shadowstream.Cipher }
-
-func (ciph *streamCipher) StreamConn(c net.Conn) net.Conn { return shadowstream.NewConn(c, ciph) }
-func (ciph *streamCipher) PacketConn(c net.PacketConn) net.PacketConn {
-	return shadowstream.NewPacketConn(c, ciph)
-}
-
 // dummy cipher does not encrypt
-
 type dummy struct{}
 
 func (dummy) StreamConn(c net.Conn) net.Conn             { return c }
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/shadowsocks/go-shadowsocks2
 go 1.12
 
 require (
-	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da
 	github.com/riobard/go-bloom v0.0.0-20200213042214-218e1707c495
 	golang.org/x/crypto v0.0.0-20190426145343-a29dc8fdc734
 )
